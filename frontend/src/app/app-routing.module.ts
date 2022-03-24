e7a7949589d1d808ac927a04ee2834e675f97a18
Flavio Kluser <flavio.kluser@stud.hslu.ch>
2022-03-24 14:07:43 +0100

    feat(frontend): minor changes as always

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -6,6 +6,12 @@ import { TimeplaceComponent } from './timeplace/timeplace.component';
 import { HomeComponent } from './home/home.component';
 import { SearchComponent } from './search/search.component';
 import { ResultsComponent } from './results/results.component';
+import { ConnectionComponent } from './connection/connection.component';
+import { ConnectionlistComponent } from './connectionlist/connectionlist.component';
+import { QueryformComponent } from './queryform/queryform.component';
+import { SearchbuttonComponent } from './searchbutton/searchbutton.component';
+import { NoresultComponent } from './noresult/noresult.component';
+import { GraphComponent } from './graph/graph.component';
 
 const routes: Routes = [
 { path: '', redirectTo: '/home', pathMatch: 'full' },
@@ -19,6 +25,12 @@ const routes: Routes = [
     SearchComponent,
     ResultsComponent,
     TimeplaceComponent,
+    ConnectionComponent,
+    ConnectionlistComponent,
+    QueryformComponent,
+    SearchbuttonComponent,
+    NoresultComponent,
+    GraphComponent,
   ],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,22 +8,11 @@ import { TimeplaceComponent } from './timeplace/timeplace.component';
 import { HomeComponent } from './home/home.component';
 import { SearchComponent } from './search/search.component';
 import { ResultsComponent } from './results/results.component';
-import { ConnectionComponent } from './connection/connection.component';
-import { ConnectionlistComponent } from './connectionlist/connectionlist.component';
-import { QueryformComponent } from './queryform/queryform.component';
-import { SearchbuttonComponent } from './searchbutton/searchbutton.component';
-import { NoresultComponent } from './noresult/noresult.component';
-import { GraphComponent } from './graph/graph.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ConnectionComponent,
-    ConnectionlistComponent,
-    QueryformComponent,
-    SearchbuttonComponent,
-    NoresultComponent,
-    GraphComponent,
   ],
   imports: [
     BrowserModule,
